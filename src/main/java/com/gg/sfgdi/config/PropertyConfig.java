bed3b6d5ff077493d6f711e3b6d8657b48dabdcb
MTocchettoP <marcostp1995@hotmail.com>
2021-03-03 10:12:23 -0700

    Added environmental variables

--- src/main/java/com/gg/sfgdi/config/PropertyConfig.java
+++ src/main/java/com/gg/sfgdi/config/PropertyConfig.java
@@ -1,15 +1,22 @@
 package com.gg.sfgdi.config;
 
 import com.gg.sfgdi.persistence.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+
+    @Autowired
+    Environment env;
+
     @Value("${gg.username}")
     String user;
 
@@ -23,7 +30,7 @@ public class PropertyConfig {
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
         fakeDataSource.setPassword(password);
-        fakeDataSource.setUrl(url);
+        fakeDataSource.setUrl(env.getProperty("ANT_HOME"));
         fakeDataSource.setUser(user);
         return fakeDataSource;
     }
